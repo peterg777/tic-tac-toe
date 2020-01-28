bf98371becdb3ec388cc3874b7af25db531c4eef
peterg777 <ptrg777@gmail.com>
2020-01-28 16:00:32 -0500

    finishing Js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let cells = document.querySelectorAll('.row > div');
-let currentPlayer = 'x';
-let moves = O;
+let currentPlayer = 'X';
+let moves = 0;
 let isGameOver = false;
 let winningCombinations = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -8,7 +8,7 @@ let winningCombinations = [
     [0, 4, 8], [6, 4, 2]
 ];
 
-for (let i = O; i < cells.length; i++) {
+for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', cellClick);
 }
 function cellClick() {
@@ -36,49 +36,52 @@ function cellClick() {
 }
 
 function checkWinner() {
-if (cells[O].textContent === 'X' && cells[1].textContent === 'X' && cells[2].textContent === 'X') {
-    console.log('X Won!');
-    isGameOVer = true;
-} else if (cells[3].textContent === 'X' && cells[4].textContent === 'X' && cells[5].textContent === 'X') {
-    console.log('X Won!');
-    isGameOVer = true;
-} else if (cells[0].textContent === 'X' && cells[3].textContent === 'X' && cells[6].textContent === 'X') {
-    console.log('X Won!');
-    isGameOVer = true;
-} else if (cells[1].textContent === 'X' && cells[4].textContent === 'X' && cells[7].textContent === 'X') {
-    console.log('X Won!');
-    isGameOVer = true;
-} else if (cells[2].textContent === 'X' && cells[5].textContent === 'X' && cells[8].textContent === 'X') {
-    console.log('X Won!');
-    isGameOVer = true;
-} else if (cells[O].textContent === 'X' && cells[4].textContent === 'X' && cells[8].textContent === 'X') {
-    console.log('X Won!');
-    isGameOVer = true;
-} else if (cells[6].textContent === 'X' && cells[4].textContent === 'X' && cells[2].textContent === 'X') {
-    console.log('X Won!');
-    isGameOVer = true;
-} else if (cells[O].textContent === 'O' && cells[1].textContent === 'O' && cells[2].textContent === 'O') {
-    console.log('O Won!');
-    isGameOVer = true;
-} else if (cells[3].textContent === 'O' && cells[4].textContent === 'O' && cells[5].textContent === 'O') {
-    console.log('O Won!');
-    isGameOVer = true;
-} else if (cells[O].textContent === 'O' && cells[3].textContent === 'O' && cells[6].textContent === 'O') {
-    console.log('O Won!');
-    isGameOVer = true;
-} else if (cells[1].textContent === 'O' && cells[4].textContent === 'O' && cells[7].textContent === 'O') {
-    console.log('O Won!');
-    isGameOVer = true;
-} else if (cells[2].textContent === 'O' && cells[5].textContent === 'O' && cells[8].textContent === 'O') {
-    console.log('O Won!');
-    isGameOVer = true;
-} else if (cells[O].textContent === 'O' && cells[4].textContent === 'O' && cells[8].textContent === 'O') {
-    console.log('O Won!');
-    isGameOVer = true;
-} else if (cells[6].textContent === 'O' && cells[4].textContent === 'O' && cells[2].textContent === 'O') {
-    console.log('O Won!');
-    isGameOVer = true;
-}
+    if (cells[0].textContent === 'X' && cells[1].textContent === 'X' && cells[2].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[3].textContent === 'X' && cells[4].textContent === 'X' && cells[5].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[0].textContent === 'X' && cells[3].textContent === 'X' && cells[6].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[1].textContent === 'X' && cells[4].textContent === 'X' && cells[7].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[2].textContent === 'X' && cells[5].textContent === 'X' && cells[8].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[0].textContent === 'X' && cells[4].textContent === 'X' && cells[8].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[6].textContent === 'X' && cells[4].textContent === 'X' && cells[2].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[0].textContent === 'O' && cells[1].textContent === 'O' && cells[2].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[3].textContent === 'O' && cells[4].textContent === 'O' && cells[5].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[0].textContent === 'O' && cells[3].textContent === 'O' && cells[6].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[1].textContent === 'O' && cells[4].textContent === 'O' && cells[7].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[2].textContent === 'O' && cells[5].textContent === 'O' && cells[8].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[0].textContent === 'O' && cells[4].textContent === 'O' && cells[8].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[6].textContent === 'O' && cells[4].textContent === 'O' && cells[2].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (moves === 9) {
+        console.log('DRAW')
+        isGameOver = true;
+    }
 }
 
 
