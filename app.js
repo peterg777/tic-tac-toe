c422809f618826f5a31f02cc406c47ef755d914e
peterg777 <ptrg777@gmail.com>
2020-01-27 20:26:43 -0500

    adding js

--- app.js
+++ app.js
@@ -1,42 +1,94 @@
 let cells = document.querySelectorAll('.row > div');
 let currentPlayer = 'x';
-let moves = 0;
+let moves = O;
 let isGameOver = false;
 let winningCombinations = [
-    [1, 2, 3],[4, 5, 6],[7, 8, 9],
-    [1, 4, 7],[2, 5, 8],[3, 6, 9],
-    [1, 5, 9],[3, 5, 7]
-  ];
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [6, 4, 2]
+];
 
-for (let i = 0; i < cells.length; i++){
+for (let i = O; i < cells.length; i++) {
     cells[i].addEventListener('click', cellClick);
 }
- function cellClick() {
-     event.target.textContent =  currentPlayer;
+function cellClick() {
 
-     togglePlayer();
- 
-     if(cells[0].textContent === 'X' || cells[1].textContent === 'X' || cells[2].textContent === 'X') {}
+    if (isGameOver === true) {
+        console.log('Game Over')
+        return;
+    }
+    // draw player
+    event.target.textContent = currentPlayer;
+    
+    // increments
+    moves++
 
- }
+    //if player won
+    checkWinner();
 
- function checkWinner(){
-     if(cells[0].textContent === 'X' || cells[1].textContent === 'X' || cells[2].textContent === 'X') {}
-        
-  }
+   // toggles for next move
+    togglePlayer();
 
- 
- 
 
- function togglePlayer() {
 
- if (currentPlayer === 'x') {
-     currentPlayer = '0';
 
- } else {
-     currentPlayer = 'x';
- }
 
 }
 
- 
+function checkWinner() {
+    if (cells[O].textContent === 'X' && cells[1].textContent === 'X' && cells[2].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[3].textContent === 'X' && cells[4].textContent === 'X' && cells[5].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[0].textContent === 'X' && cells[3].textContent === 'X' && cells[6].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[1].textContent === 'X' && cells[4].textContent === 'X' && cells[7].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[2].textContent === 'X' && cells[5].textContent === 'X' && cells[8].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[O].textContent === 'X' && cells[4].textContent === 'X' && cells[8].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[6].textContent === 'X' && cells[4].textContent === 'X' && cells[2].textContent === 'X') {
+        console.log('X Won!');
+        isGameOVer = true;
+    } else if (cells[O].textContent === 'O' && cells[1].textContent === 'O' && cells[2].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[3].textContent === 'O' && cells[4].textContent === 'O' && cells[5].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[O].textContent === 'O' && cells[3].textContent === 'O' && cells[6].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[1].textContent === 'O' && cells[4].textContent === 'O' && cells[7].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[2].textContent === 'O' && cells[5].textContent === 'O' && cells[8].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[O].textContent === 'O' && cells[4].textContent === 'O' && cells[8].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    } else if (cells[6].textContent === 'O' && cells[4].textContent === 'O' && cells[2].textContent === 'O') {
+        console.log('O Won!');
+        isGameOVer = true;
+    }
+
+
+
+
+function togglePlayer() {
+
+        if (currentPlayer === 'X') {
+            currentPlayer = 'O';
+
+        } else {
+            currentPlayer = 'X';
+        }
+    }
